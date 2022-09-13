3c395eb189f3f2a9123125bc9d382a30752f7762
Administrator <admin@example.com>
2022-09-13 16:09:30 +0800

    '若依框架系统改造'

---
+++ wolfcode-car-admin/src/main/java/com/wolfcode/car/web/controller/system/SysLoginController.java
@@ -0,0 +1,87 @@
+package com.wolfcode.car.web.controller.system;
+
+import java.util.List;
+import java.util.Set;
+
+import com.wolfcode.car.common.core.domain.AjaxResult;
+import com.wolfcode.car.common.core.domain.entity.SysMenu;
+import com.wolfcode.car.common.core.domain.entity.SysUser;
+import com.wolfcode.car.common.core.domain.model.LoginBody;
+import com.wolfcode.car.system.service.ISysMenuService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.wolfcode.car.common.constant.Constants;
+import com.wolfcode.car.common.utils.SecurityUtils;
+import com.wolfcode.car.framework.web.service.SysLoginService;
+import com.wolfcode.car.framework.web.service.SysPermissionService;
+
+/**
+ * 登录验证
+ * 
+ * @author ruoyi
+ */
+@RestController
+public class SysLoginController
+{
+    @Autowired
+    private SysLoginService loginService;
+
+    @Autowired
+    private ISysMenuService menuService;
+
+    @Autowired
+    private SysPermissionService permissionService;
+
+    /**
+     * 登录方法
+     * 
+     * @param loginBody 登录信息
+     * @return 结果
+     */
+    @PostMapping("/login")
+    public AjaxResult login(@RequestBody LoginBody loginBody)
+    {
+        AjaxResult ajax = AjaxResult.success();
+        // 生成令牌
+        String token = loginService.login(loginBody.getUsername(), loginBody.getPassword(), loginBody.getCode(),
+                loginBody.getUuid());
+        ajax.put(Constants.TOKEN, token);
+        return ajax;
+    }
+
+    /**
+     * 获取用户信息
+     * 
+     * @return 用户信息
+     */
+    @GetMapping("getInfo")
+    public AjaxResult getInfo()
+    {
+        SysUser user = SecurityUtils.getLoginUser().getUser();
+        // 角色集合
+        Set<String> roles = permissionService.getRolePermission(user);
+        // 权限集合
+        Set<String> permissions = permissionService.getMenuPermission(user);
+        AjaxResult ajax = AjaxResult.success();
+        ajax.put("user", user);
+        ajax.put("roles", roles);
+        ajax.put("permissions", permissions);
+        return ajax;
+    }
+
+    /**
+     * 获取路由信息
+     * 
+     * @return 路由信息
+     */
+    @GetMapping("getRouters")
+    public AjaxResult getRouters()
+    {
+        Long userId = SecurityUtils.getUserId();
+        List<SysMenu> menus = menuService.selectMenuTreeByUserId(userId);
+        return AjaxResult.success(menuService.buildMenus(menus));
+    }
+}
