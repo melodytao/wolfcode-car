6545ab1e7b200b76a543d39ac50b372b6f678a45
linzhaoliang <linzhaoliang@wolfcode.cn>
2022-09-15 16:56:57 +0800

    '日志管理-操作日志完成'

---
+++ wolfcode-car-admin/src/main/java/com/wolfcode/car/web/controller/monitor/SysOperLogController.java
@@ -0,0 +1,55 @@
+package com.wolfcode.car.web.controller.system;
+
+import com.wolfcode.car.common.annotation.Log;
+import com.wolfcode.car.common.core.controller.BaseController;
+import com.wolfcode.car.common.core.domain.AjaxResult;
+import com.wolfcode.car.common.core.page.TableDataInfo;
+import com.wolfcode.car.common.enums.BusinessType;
+import com.wolfcode.car.common.utils.poi.ExcelUtil;
+import com.wolfcode.car.system.domain.SysOperLog;
+import com.wolfcode.car.system.service.ISysOperLogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+@RestController
+@RequestMapping("/monitor/operlog")
+public class SysOperLogController extends BaseController {
+    @Autowired
+    private ISysOperLogService operLogService;
+
+    @PreAuthorize("@ss.hasPermi('monitor:operlog:list')")
+    @GetMapping("/list")
+    public TableDataInfo list(SysOperLog operLog) {
+        startPage();
+        List<SysOperLog> list = operLogService.selectOperLogList(operLog);
+        return getDataTable(list);
+    }
+
+    @Log(title = "操作日志", businessType = BusinessType.EXPORT)
+    @PreAuthorize("@ss.hasPermi('monitor:operlog:export')")
+    @PostMapping("/export")
+    public void export(HttpServletResponse response, SysOperLog operLog) {
+        List<SysOperLog> list = operLogService.selectOperLogList(operLog);
+        ExcelUtil<SysOperLog> util = new ExcelUtil<>(SysOperLog.class);
+        util.exportExcel(response, list, "操作日志");
+    }
+
+    @Log(title = "操作日志", businessType = BusinessType.DELETE)
+    @PreAuthorize("@ss.hasPermi('monitor:operlog:remove')")
+    @DeleteMapping("/{operIds}")
+    public AjaxResult remove(@PathVariable Long[] operIds) {
+        return toAjax(operLogService.deleteOperLogByIds(operIds));
+    }
+
+    @Log(title = "操作日志", businessType = BusinessType.CLEAN)
+    @PreAuthorize(("@ss.hasPermi('monitor:operlog:remove')"))
+    @DeleteMapping("/clean")
+    public AjaxResult clean() {
+        operLogService.cleanOperLog();
+        return AjaxResult.success();
+    }
+}
